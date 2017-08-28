239475bf43eaa95a4c17fa466528e36c438b47ee
headhunter94 <30793606+headhunter94@users.noreply.github.com>
2017-08-28 12:52:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.get('/article-one', function(req, res){
 });
 
 app.get('/article-two', function(req, res){
-   res.send('Article two requested and ill be served'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-otwo.html'));
     
 });
 
 app.get('/article-three', function(req, res){
-   res.send('Article three requested and ill be served'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
     
 });
 
