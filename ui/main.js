39b227e4c5958f1a4290557387cf57d82db10ccc
headhunter94 <30793606+headhunter94@users.noreply.github.com>
2017-08-28 18:24:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value' ;
 
 var img = document.getElementById('madi'); 
+marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft +'px';
+}
   img.onclick = function() {
-img.style.marginLeft = '100px';
+  var interval = setInterval(moveRight, 50);
+  img.style.marginLeft = '100px';
 };
