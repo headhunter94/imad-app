7a25632f7a7d5136ee9468e5afc170203778f3ab
headhunter94 <30793606+headhunter94@users.noreply.github.com>
2017-08-29 22:39:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg');
+
+var config = {
+    user: 'madweshmahith',
+    database: 'madweshmahith',
+    host: 'http://db.imad.hasura-app.io',
+    port: '5432'
+    password: process,env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -85,6 +94,19 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+ var pool = new Pool(config);
+app.get('/test-db', function(req, res){
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+            
+            
+        }if (else) {
+            res.send(JSON.stringify(result));
+        }
+    })
 });
 
 app.get('/:articleName', function(req, res){
